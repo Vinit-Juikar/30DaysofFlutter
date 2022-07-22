7d3f15e3cb7b4752ca3b871695425fa015c068d9
Vinit Juikar <juikarvinit123@gmail.com>
2022-07-22 19:14:27 +0530

    this is work for day2

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,20 +2,20 @@ import 'package:flutter/material.dart';
 
 final int days = 30;
 final String name = "Vinit";
- 
- class Homepage extends StatelessWidget {
 
+class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return 
-      Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to $days days of $name"),
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of $name"),
         ),
-      );
-    
-    
+      ),
+      drawer: Drawer(),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,12 @@ import 'package:flutter_application_1/home_page.dart';
 
 void main() => runApp(MyApp());
 
-
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home:Homepage() 
-    );
+    return MaterialApp(home: Homepage());
   }
 }
+ 
