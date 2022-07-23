6db24ae7749170f110455a62660c982bc61867ef
Vinit Juikar <juikarvinit123@gmail.com>
2022-07-23 18:35:13 +0530

    this is work done by me on day4

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key); // future use, no such use here
 
-  @override
+  @override // functiond are already defined so we use override to make modifications in them
   Widget build(BuildContext context) {
     return MaterialApp(
+      // without this there won't be any output
       // home: Homepage(), this homepage is not to be defined if it is so then it will throw error as it is defined twice
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ), // used to decide the theme of the font that we are using
       darkTheme: ThemeData(brightness: Brightness.dark),
       routes: {
-        "/": (context) => Homepage(),
-        "/login": (context) => LoginPage(),
+        "/": (context) => LoginPage(),
+        // "/login": (context) => LoginPage(),
       },
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/material.dart ';
+import 'dart:html';
+import 'dart:ui';
+
+import 'package:flutter/material.dart '; // add everytime we start with the app
 
 class LoginPage extends StatelessWidget {
   // const login_page({Key? key}) : super(key: key);
@@ -6,9 +9,21 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("Login Page"),
-      ),
-    );
+        color: Colors
+            .white, // this is mostly used to make the image match with the background
+        child: Column(
+          children: [
+            // in a case where there are multiple small groups in a section we use children
+            Image.asset(
+              // here this property is used to add image in application
+              "assets/image/undraw_secure_login_pdn4.png",
+              fit: BoxFit.cover,
+            ),
+            Text("Welcome to the app",
+                style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold)), // main use of this is styling the text that we have written
+          ],
+        ));
   }
 }
