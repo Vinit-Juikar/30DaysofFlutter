b8b0491a1b585a690db729ef50d50fccc604a2cd
Vinit Juikar <juikarvinit123@gmail.com>
2022-08-01 18:41:27 +0530

    day_8 work has been done

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,8 +1,6 @@
-
 // ignore_for_file: prefer_const_constructors, duplicate_ignore, use_key_in_widget_constructors
 // import 'dart:ffi';
 // import 'dart:ui';
-
 
 import 'package:flutter/material.dart ';
 import 'package:flutter/material.dart';
@@ -16,10 +14,30 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String name =
+      ""; // thsi is done to add name after the name is written in username
+  bool changeButton = false;
 
-  String name =""; // thsi is done to add name after the name is written in username
-bool changeButton = false;
   // const login_page({Key? key}) : super(key: key);
+  final _formKey = GlobalKey<FormState>();
+
+
+
+  moveToHome(BuildContext context) async {
+    // if ( _formKey.currentState.validate()) { // here i faced issue do to validate statement ... just need to resolve that 
+      setState(() {
+        changeButton = true;
+      });
+
+      await Future.delayed(Duration(seconds: 1));
+
+      await Navigator.pushNamed(context, MyRoutes.homeRoute);
+      setState(() {
+        changeButton = false;
+      });
+    // }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -36,97 +54,92 @@ bool changeButton = false;
                 height: 200,
               ),
               // ignore: prefer_const_constructors
-              Text("Welcome $name",
-                  // ignore: prefer_const_constructors
-                  style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      ),),
+              Text(
+                "Welcome $name",
+                // ignore: prefer_const_constructors
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
               // ignore: prefer_const_constructors
               SizedBox(
-                height:20.0,
+                height: 20.0,
                 // ignore: prefer_const_constructors
                 // child:Text(style: TextStyle(color: Colors.black),"there is something that we are missinng on")
               ),
               Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-                child: Column(children: [
-                  TextFormField(
-                  // ignore: prefer_const_constructors
-                  decoration: InputDecoration(
-                    hintText: "username",
-                    labelText: "User"
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 32.0),
+                child: Form(
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        // ignore: prefer_const_constructors
+                        decoration: InputDecoration(
+                          hintText: "Enter username",
+                          labelText: "Username",
+                        ),
+                        validator: (String? value) {
+                          if (value != null && value.isEmpty) {
+                            return "Username cannot be empty";
+                          } else
+                            return null;
+                        },
+                        onChanged: (value) {
+                          // this component is used as a call back to when we change anything it will reflect to us
+                          name = value;
+                          setState(() {});
+                        },
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            hintText: "Password", labelText: "Enter password"),
+                        validator: (String? value) {
+                          if (value != null && value.isEmpty) {
+                            return "Password cannot be empty";
+                          } else if (value != null && value.length < 6) {
+                            return "Password length is too short ";
+                          } else
+                            return null;
+                        },
+                      ),
+                      SizedBox(
+                        height: 50.0,
+                        // ignore: prefer_const_constructors
+                      ),
+                      Material(
+                        borderRadius:
+                            BorderRadius.circular(changeButton ? 50 : 150),
+                        color: Colors.deepPurple,
+                        child: InkWell(
+                          onTap: () => moveToHome(context),
+                          child: AnimatedContainer(
+                            duration: Duration(seconds: 1),
+                            height: 50,
+                            width: changeButton ? 50 : 150,
+                            alignment: Alignment.center,
+                            child: changeButton
+                                ? Icon(
+                                    Icons.done,
+                                    color: Colors.white,
+                                  )
+                                : Text(
+                                    "Login",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18),
+                                  ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  onChanged: (value) { // this component is used as a call back to when we change anything it will reflect to us
-                  name = value;
-                  setState(() {
-                    
-                  });},
                 ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: "Password",
-                    labelText: "Enter password"
-                  ),
-                ),
-                 SizedBox(
-                height:50.0,
-                // ignore: prefer_const_constructors
-               
-              ),
-
-InkWell(
-  onTap: () async {
-    setState(() {
-      changeButton = true;
-      
-
-    });
-    await Future.delayed(Duration(seconds: 1));
-    Navigator.pushNamed(context, MyRoutes.homeRoute);
-  },
-  child:   AnimatedContainer(
-  duration: Duration(seconds: 1),
-    height: 50,
-  
-    width: changeButton? 50: 150,
-  
-  
-  
-    alignment: Alignment.center,
-    child: changeButton? Icon(
-      Icons.done,
-      color: Colors.white,
-    )
-  
-     : Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
-  
-    ),
-  
-    decoration: BoxDecoration(
-      // shape: changeButton? BoxShape.circle: BoxShape.rectangle,
-  
-   color: Colors.deepPurple,
-  
-   borderRadius: BorderRadius.circular(changeButton? 50: 8)
-  
-    ),
-  
-  ),
-),
-
-
-              // ignore: sort_child_properties_last
-              // ElevatedButton(child: Text("Login"),
-              // style: TextButton.styleFrom(minimumSize: Size(150,45)),//most of the time you won't get any req output so consider it as width and height by default
-              // onPressed: (){
-              //   
-          
-              // },),
-                ],),
               )
-               // main use of this is styling the text that we have written
+              // main use of this is styling the text that we have written
             ],
           ),
         ));
